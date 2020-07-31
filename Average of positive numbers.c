067ced8818f110ee43010fe7103489faa79a416a
ripan-sidhu <63389710+ripan-sidhu@users.noreply.github.com>
2020-07-31 23:56:48 +0530

    practice of basic declaration and expressions

---
+++ Average of positive numbers.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	float a[5];
+	float avg;
+	int n=0;
+	float t=0;
+	int j;
+	printf("Enter First Number");
+	scanf("%f",&a[1]);
+	printf("Enter Second Number");
+	scanf("%f",&a[2]);
+	printf("Enter Third Number");
+	scanf("%f",&a[3]);
+	printf("Enter Fourth Number");
+	scanf("%f",&a[4]);
+	printf("Enter Fifth Number");
+	scanf("%f",&a[5]);
+	
+	for(j=0;j<5;j++)
+	{
+		if(a[j]>0)
+		{
+			n++;
+			t=t+a[j];
+		}
+	}
+	avg=t/n;
+	
+	printf("%d\n",n);
+	
+	printf("%2f",avg);
+	
+}
