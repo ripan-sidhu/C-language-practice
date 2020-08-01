dd9ee8096ca0e1d8edaf927a3655041d91de1569
ripan-sidhu <63389710+ripan-sidhu@users.noreply.github.com>
2020-08-02 00:03:23 +0530

    practce of basic declaration and expressions

---
+++ enter five numbers and find sum of all odd values .c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int a[5];
+	int i,p=0;
+	int s=0;
+	printf("Enter First Number");
+	scanf("%d",&a[0]);
+	printf("Enter Second Number");
+	scanf("%d",&a[1]);
+	printf("Enter Third Number");
+	scanf("%d",&a[2]);
+	printf("Enter Fourth Number");
+	scanf("%d",&a[3]);
+	printf("Enter Fifth Number");
+	scanf("%d",&a[4]);
+	
+    for(i=0;i<5;i++)
+    {
+	  if(a[i] % 2!=0)
+	  {
+	  	s=s+a[i];
+	 
+	  }
+	  
+    }
+     	printf("%d",s);
+}
