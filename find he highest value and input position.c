8edccdd9712e29ba96c50d1b871e47c9abf04093
ripan-sidhu <63389710+ripan-sidhu@users.noreply.github.com>
2020-08-05 23:59:17 +0530

    practice of basic declaration and expressions

--- find he highest value and input position.c
+++ find he highest value and input position.c
@@ -3,11 +3,12 @@ int main()
 { int s,i;   
 printf("How many numbers you want to enter?\n");
   scanf("%d",&s);
-  printf("Enter the Numbers:\n");
+  printf("Enter the Numbers:-\n");
 	int a[s];
 	for(i=0;i<s;i++)
 	{
 		scanf("%d",&a[i]);
+		
 	}
 	
 }
